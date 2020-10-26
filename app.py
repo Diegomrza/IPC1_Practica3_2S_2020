2c05b87194e7d8993534c96b8b8fa36988fe03d6
Diegomrza <2993323220101@ingenieria.usac.edu.gt>
2020-10-25 20:41:35 -0600

    .
    .

--- app.py
+++ app.py
@@ -5,18 +5,26 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/operaciones', methods=['POST'])
+@app.route('/suma', methods=['POST'])
 def sumar():
-    if request.method == 'POST':
 
-        numero1 = request.form.get('numero_uno')
-        numero2 = request.form.get('numero_dos')
-        suma = Operaciones()
-        suma.suma(numero1, numero2)
+    return 'Suma' 
 
-        return 'La suma es ', suma 
+@app.route('/resta', methods=['POST'])
+def restar():
 
-    return "Hola"    
+    return 'Resta' 
+
+@app.route('/multiplicacion', methods=['POST'])
+def multiplicar():
+
+    return 'Multi' 
+   
+
+@app.route('/division', methods=['POST'])
+def dividir():
+
+    return 'Divi' 
 
 if __name__ == "__main__":
 	app.run(threaded = True,port = 3000, debug = True)
